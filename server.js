ca5c13d52ab38406b380d22137c9aed3f4a86c6a
SKITVN <starskingitphone@gmail.com>
2026-08-09 21:09:58 +0700

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const { builder } = require("./addon");
-
-const port = Number(process.env.PORT || 7000);
-serveHTTP(builder.getInterface(), { port, cache: "1h" });
-console.log(`KKPhim Stremio addon listening on ${port}`);
